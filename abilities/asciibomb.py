45e1738cc6d33051252f3d75ff59cca9ee5d82d0
Adam Kessler <akessler@gmail.com>
2016-08-30 07:25:08 -0400

    working on it...

--- abilities/asciibomb.py
+++ abilities/asciibomb.py
@@ -1,6 +1,25 @@
 
 
+def asciibomb(dictRef):
+    data = dictRef['data'].split(' ', 2)
+    i = 0
+    el_ascii = data[2]
+    art_file = "/home/members/akessler/bots/demonIRCBot/abilities/art/{f}".format(f=el_ascii)
+    f = open(art_file)
+    lines = f.readlines()
+    for less in dictRef['bot'].lessers:
+        dictRef['bot'] = less
+        less.talk(dictRef['where'], lines[i])
+        i += 1
+    f.close()
+
 # def asciibomb(dictRef):
+#     data = dictRef['data'].split(' ', 4)
+#     if len(data) < 4:
+#         print(dictRef)
+#
+#
+# def printer(dictRef):
 #     data = dictRef['data'].split(' ', 4)
 #     i = 0
 #     el_ascii = data[2]
@@ -12,22 +31,3 @@
 #         less.talk(dictRef['where'], lines[i])
 #         i += 1
 #     f.close()
-
-def asciibomb(dictRef):
-    data = dictRef['data'].split(' ', 4)
-    if len(data) < 4:
-        print(dictRef)
-
-
-def printer(dictRef):
-    data = dictRef['data'].split(' ', 4)
-    i = 0
-    el_ascii = data[2]
-    art_file = "/home/members/akessler/bots/demonIRCBot/abilities/art/{f}".format(f=el_ascii)
-    f = open(art_file)
-    lines = f.readlines()
-    for less in dictRef['bot'].lessers:
-        dictRef['bot'] = less
-        less.talk(dictRef['where'], lines[i])
-        i += 1
-    f.close()
