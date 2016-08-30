3a1e7584538fbc84549d39e55e24e8860f5fc77b
Adam Kessler <akessler@gmail.com>
2016-08-30 06:09:57 -0400

    working on it...

--- abilities/asciibomb.py
+++ abilities/asciibomb.py
@@ -8,11 +8,14 @@
 
 def asciibomb(dictRef):
     data = dictRef['data'].split(' ', 2)
+    i = 0
     el_ascii = "dice"
     art_file = "./art/{f}".format(f=el_ascii)
     print('attempting to ascii')
     f = open('/home/members/akessler/bots/demonIRCBot/abilities/art/dice')
+    lines = f.readlines()
     for less in dictRef['bot'].lessers:
         dictRef['bot'] = less
-        less.performAbility('echo', line)
+        less.talk(dictRef['where'], lines[i])
+        i += 1
     f.close()
