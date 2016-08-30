010646a50326279bc705c9da6aab4f8775af0d65
Adam Kessler <akessler@gmail.com>
2016-08-30 04:34:32 -0400

    working on it...

--- abilities/broadcast.py
+++ abilities/broadcast.py
@@ -5,8 +5,9 @@ def broadcast(dictRef):
     ability = data[1]
     # print('attempting to broadcast ability: ' + ability)
     # print('with data: ' + str(dictRef))
-    print(less)
+
     dictRef['bot'].performAbility(ability, dictRef)
     for less in dictRef['bot'].lessers:
         dictRef['bot'] = less
         less.performAbility(ability, dictRef)
+        print(less)
