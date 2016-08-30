e45e4e8eda30fcb40b2a435acbe1c210e845b5bc
Adam Kessler <akessler@gmail.com>
2016-08-30 11:18:59 -0400

    FIRE THE ASCII BOMB!

--- start2.py
+++ start2.py
@@ -2,6 +2,6 @@ import bots
 
 #This should be the "master" bot who has all abilities and only one owner, which is the controller, aka me/yourself
 #if you want more bots you must spawn more bots from this master bot
-b1 = bots.Bot(name="kid_rush2", thrId=2, ablBits=(-1 & 0xFFFFFFFF), channels='#rush')
+b1 = bots.Bot(name="kid_rush", thrId=2, ablBits=(-1 & 0xFFFFFFFF), channels='#nospace')
 #overrides the thread.start method so that it is a threaded bot
 b1.start()
