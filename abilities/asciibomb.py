3326e08bdba1369e2edfe83f537f4eda0b237f65
Adam Kessler <akessler@gmail.com>
2016-08-30 19:42:23 -0400

    FIRE THE ASCII BOMB!

--- abilities/asciibomb.py
+++ abilities/asciibomb.py
@@ -1,26 +1,9 @@
 import time
 
-# def asciibomb(dictRef):
-#     data = dictRef['data'].split(' ', 4)
-#     i = 0
-#     el_ascii = data[2]
-#     art_file = "/home/members/akessler/bots/demonIRCBot/abilities/art/{f}".format(f=el_ascii)
-#     f = open(art_file)
-#     lines = f.readlines()
-#     for less in dictRef['bot'].lessers:
-#         dictRef['bot'] = less
-#         less.talk(dictRef['where'], lines[i])
-#         i += 1
-#     f.close()
 
 def asciibomb(dictRef):
     topDog = dictRef['bot']
     data = dictRef['data'].split(' ', 4)
-    # d = data[3]
-    # print(data[4])
-    # print(d.startswith('#'))
-    # if data[4] == '-pm':
-    #     printer2(dictRef, dictRef['bot'])
     if len(data) > 3:
         jdata = "x {c}".format(c=data[3])
         dictRef['bot'].joinChan(data[3])
@@ -34,8 +17,6 @@ def asciibomb(dictRef):
         printer(dictRef, dictRef['bot'])
         dictRef['bot'] = topDog
         time.sleep(1)
-        # for i in range(10000000):
-        #     j = 1+1+3+635-123
         for less in dictRef['bot'].lessers:
             dictRef['bot'] = less
             less.leave(data[3])
@@ -57,28 +38,22 @@ def printer(dictRef, top):
         say_in = dictRef['where']
     f = open(art_file)
     lines = f.readlines()
-    # for less in top.lessers:
-    #     dictRef['bot'] = less
-    #     less.talk(say_in, lines[i])
-    #     i += 1
-    #     if len(lines) == i:
-    #         break
+
     while i < range(len(lines)):
         for less in top.lessers:
-            # top = less
             less.talk(say_in, lines[i])
             i += 1
             if len(say_in) > 6:
                 time.sleep(0.05)
-            if i%29 == 0:
-                time.sleep(1)
+            if i%29 == 0 and len(lines) > 30:
+                time.sleep(2)
             if len(lines) == i:
                 break
         if len(lines) == i:
             break
     f.close()
 
-#
+
 # def printer2(dictRef, top):
 #     data = dictRef['data'].split(' ', 4)
 #     i = 0
