d5d8741e2aa874395964be2b7d8a3612b0a223d9
Adam Kessler <akessler@gmail.com>
2016-09-04 21:13:32 -0400

    need a msg

--- abilities/getem.py
+++ abilities/getem.py
@@ -1,7 +1,16 @@
+import time
 
 
-def getem(dictRef)
+def getem(dictRef):
     data = dictRef['data'].split(' ', 3)
     top = dictref['bot']
+    i = 'a'
     while True:
-        performAbility(createswarm, data[4])
+
+        dictRef['bot'].performAbility(tell, "pilar: tell pilar zag: spawnoverlord  60 {j}".format(j=i)
+        # dictRef['bot'].performAbility(tell, "zag: spawnoverlord 60 {j}".format(j=i)
+        # time.sleep(.02)
+        # dictRef['bot'].performAbility(tell, "overlord{j}: tell {n} test".format(j=i, n=data[2]))
+        # time.sleep(70)
+        # dictRef['bot'].performAbility(tell, "{t}".format(t=top))
+        i += 1
