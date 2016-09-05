68a52e9d8ca1ea170de1587f75599f40d44cb48f
Adam Kessler <akessler@gmail.com>
2016-09-04 21:22:50 -0400

    need a msg

--- abilities/getem.py
+++ abilities/getem.py
@@ -5,13 +5,15 @@ def getem(dictRef):
     data = dictRef['data'].split(' ', 3)
     top = dictref['bot']
     i = 0
-    while True:
+    a = 1
+    while a == 1:
 
-        dictRef['bot'].performAbility(tell, "zag: tell pilar zag: spawnoverlord  60" #{j}".format(j=i)
+        dictRef['bot'].performAbility(tell, "zag: tell pilar zag: spawnoverlord  60 {j}".format(j=i)
         # dictRef['bot'].performAbility(tell, "zag: spawnoverlord 60 {j}".format(j=i)
         # time.sleep(.02)
         # dictRef['bot'].performAbility(tell, "overlord{j}: tell {n} test".format(j=i, n=data[2]))
         # time.sleep(70)
         # dictRef['bot'].performAbility(tell, "{t}".format(t=top))
-        # i = i+1
-        
+        i = i+1
+        if i == 120:
+            a = 0
