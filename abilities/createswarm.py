80da3129ff1016b5e8e9b268bbcc0c158563b1e5
Adam Kessler <akessler@gmail.com>
2016-09-04 20:34:25 -0400

    need a msg

--- abilities/createswarm.py
+++ abilities/createswarm.py
@@ -57,4 +57,40 @@ def createswarm(dictRef):
             else:
                 dictRef['bot'].talk(dictRef['where'], "{d} is not a number".format(d=data[2]))
     else:
-        dictRef['bot'].talk(dictRef['where'], "There already exists a swarm")
+        origin = dictRef['bot']
+        data = dictRef['data'].split(' ', 5)
+        ld = len(data)
+        modif = data[ld-1]
+        if(len(data) < 3):
+            size = 10
+        else:
+            if(data[2].isdigit()):
+                size = int(data[2])
+                if size < 100:
+                    if(len(data) < 4):
+                        swarmlingNames = 'ling{m}'.format(m=modif)
+                    else:
+                        swarmlingNames = data[ld - 1]
+                    if(len(swarmlingNames) < 10):
+
+                        #first createimp called swarm
+                        swarmRef = dictRef
+
+                        swarmRef['data'] = '{n} createimp {s}'.format(n=dictRef['bot'].name, s='overlord{m}'.format(m=modif))
+                        origin.performAbility('createimp', swarmRef)
+                        #then createimp x times with swarm
+                        swarmlingRef = swarmRef
+                        swarmlingRef['bot'] = findSwarmBot(origin)
+                        for x in xrange(size):
+                            swarmlingRef['data'] = '{n} createimp {s}'.format(n='overlord', s=swarmlingNames+str(x+1))
+                            swarmlingRef['bot'].performAbility('createimp', swarmlingRef)
+#                            time.sleep(2)
+
+                        #then set variable
+                        swarmlingRef['bot'].isSwarm = True
+                    else:
+                        dictRef['bot'].talk(dictRef['where'], "Too large of swarmling name, needs to be 9 or less ")
+                else:
+                    dictRef['bot'].talk(dictRef['where'], "Must be of size 50 or less")
+            else:
+                dictRef['bot'].talk(dictRef['where'], "{d} is not a number".format(d=data[2]))
