e1ba026256c4406f0c076f1790bf62fde741b4c1
Adam Kessler <akessler@gmail.com>
2016-09-06 19:38:44 -0400

    need a msg

--- abilities/abilityhandler.py
+++ abilities/abilityhandler.py
@@ -58,7 +58,7 @@ class Abilities:
         'asciibomb':        0b00000000100000000000000000000000, #WIP
         'asciiname':        0b00000001000000000000000000000000,  # WIP
         'getem':            0b00000010000000000000000000000000,
-        'cname':            0b00000100000000000000000000000000,'  # WIP
+        'cname':            0b00000100000000000000000000000000,  # WIP
         #        'spam':             0b00000000010000000000000000000000, #pulls random spam line from file
 #        'macro':             0b00000000010000000000000000000000, #macros are a defined ability created from a collection of other abilities
         }
